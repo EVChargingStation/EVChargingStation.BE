741aa0db6c84398b75d63f592a5c06851b995f95
Ch1mpleo <vietanh051203@gmail.com>
2025-09-12 01:56:14 +0700

    chore: Update .gitignore and assembly info files

--- EVChargingStation.API/obj/Debug/net8.0/EVChargingStation.API.AssemblyInfo.cs
+++ EVChargingStation.API/obj/Debug/net8.0/EVChargingStation.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVChargingStation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bae349ff8fdd7d76f99563de81b5467b56a725")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVChargingStation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVChargingStation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EVChargingStation.Domain/obj/Debug/net8.0/EVChargingStation.Domain.AssemblyInfo.cs
+++ EVChargingStation.Domain/obj/Debug/net8.0/EVChargingStation.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVChargingStation.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bae349ff8fdd7d76f99563de81b5467b56a725")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVChargingStation.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVChargingStation.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
